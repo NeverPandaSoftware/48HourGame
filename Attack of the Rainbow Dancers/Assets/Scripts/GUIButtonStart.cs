2b867bdee92975183612eddbe70bcf0a10f6412e
TheOnlyHombre <barto94@gmail.com>
2014-10-17 20:37:53 -0500

    Menu Files
    Added start menu script, button script, and scenes

---
+++ Attack of the Rainbow Dancers/Assets/Scripts/GUIButtonStart.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIButtonStart : MonoBehaviour 
+{
+    public Texture2D texture = null;
+
+    private void OnGUI()
+    {
+        if(GUI.Button (new Rect(Screen.width / 2 - 63, Screen.height / 2 - 63, texture.width, texture.height), texture))
+        {
+            Application.LoadLevel("Level1);"
+        }
+
+    }
+
+}
