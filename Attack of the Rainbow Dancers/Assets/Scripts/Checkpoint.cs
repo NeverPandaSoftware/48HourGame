220a11c3ac2a0abc7ac06391480d1c34015d5180
felrya <felrya92@live.com>
2014-10-18 12:01:31 -0500

    Version 0.6

---
+++ Attack of the Rainbow Dancers/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using SynchronizerData;
+
+public class Checkpoint : MonoBehaviour
+{
+    public float audioTime;
+    public bool activated = false;
+    private BeatObserver beatObserver;
+    public GameObject audioSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        beatObserver = GetComponent<BeatObserver>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if ((beatObserver.beatMask & BeatType.DownBeat) == BeatType.DownBeat)
+        {
+            audioTime = audioSource.audio.time;
+            Debug.Log("Current time: " + audioTime);
+        }
+	}
+}
